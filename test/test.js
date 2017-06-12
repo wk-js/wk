8ec907c0e8a75693debed8d3033e870b9fa3958c
Christophe Massolin <christophe.massolin@makemepulse.com>
2017-06-12 15:51:37 +0200

    Fix Task2 implementation

--- test/test.js
+++ test/test.js
@@ -1,39 +1,34 @@
 
 const wk = require('../lib/workflow')
 
-task('message', { argv: { name: 'John' } }, function() {
+wk.Print.silent()
+
+task('hello', { argv: { name: 'John' } }, function() {
   console.log( 'Hello', this.argv.name, '!' )
   return this.argv.name
 })
 
-task('plouf', { argv: { name: 'Max' } }, function() {
+task('salut', { argv: { name: 'Max' } }, function() {
   console.log( 'Salut', this.argv.name, '!' )
   return this.argv.name
 })
 
-// const TaskManager = require('../lib/task-manager')
-// const Task = require('../lib/tasks/task2')
+task('message', function() {
+  console.log(this.argv.name+':', this.argv.message)
+})
 
-// var task0 = new Task('message', {
-//   argv: { name: 'John' },
-//   action: function() {
-    // console.log( 'Hello', this.argv.name, '!' )
-    // return this.argv.name
-//   }
-// })
+task('thinking', { async: true }, function() {
+  console.log(this.argv.name, 'thinking...')
+  setTimeout(() => {
+    this.complete()
+  }, 1000)
+})
 
-// var task1 = new Task('plouf', {
-//   argv: { name: 'Max', arr: [ 'lol' ] },
-//   action: function() {
-//     console.log( 'Salut', this.argv.name, '!', this.argv )
-//     return this.argv.name
-//   }
-// })
-
-// TaskManager.serie(task0, task1).then(function(res) {
-//   console.log('-------------------')
-//   console.log('Results', res)
-// })
-
-// // task1.execute({ argv: { name: 'Pierre' } })
-// // task1.execute({ argv: { arr: [ 'plouf' ] } })
+serie(
+  'hello',
+  'salut',
+  'thinking --name Max',
+  'message --name Max --message "How are you ?"',
+  'thinking --name John',
+  'message --name John --message "Je vais bien et toi ?"'
+)
