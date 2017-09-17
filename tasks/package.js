f344868c8e7f5df896094520dcd2246af2f3b0d1
Christophe Massolin <christophe.massolin@gmail.com>
2017-09-17 04:49:44 +0200

    Recreate package and publish task. (Found ExtraTask alternative with invoke() ?)

---
+++ tasks/package.js
@@ -0,0 +1,95 @@
+'use strict'
+
+const { template } = require('lol/utils/string')
+const path     = require('path')
+const when     = require('when')
+const FileList = require('filelist').FileList
+const fs       = require('fs-extra')
+
+
+const COMMANDS = {
+  zip:   "zip -r ${path}.zip ${path}",
+  gzip:  "tar -c ${path} | gzip -9 -> ${path}.tar.gz",
+  bzip2: "tar -cjf ${path}.tar.bz2 ${path}",
+}
+
+function getPackagePath( subtask ) {
+  return path.join( subtask.argv.pkg_path, subtask.argv.name )
+}
+
+function tasks(subtask) {
+
+  let FL
+
+  return {
+    "clean": function(resolve, reject) {
+      const p = getPackagePath( subtask )
+
+      fs.remove( p, (err) => {
+        if (err) reject( err )
+        else resolve()
+      })
+    },
+
+    "filelist": function(resolve) {
+      FL = new FileList
+
+      if (subtask.argv.include) {
+        subtask.argv.include.split(',').forEach((inc) => {
+          FL.include(inc)
+        })
+      }
+
+      if (subtask.argv.exclude) {
+        subtask.argv.exclude.split(',').forEach((exc) => {
+          FL.exclude(exc)
+        })
+      }
+
+      resolve(FL.toArray())
+    },
+
+    "copy": function(resolve, reject) {
+      const p = getPackagePath( subtask )
+
+      when.reduce(this.result, function( res, file ) {
+        return when.promise(function( res ) {
+          fs.copy( file, path.join( p, file ), fs.constants.COPYFILE_EXCL, res )
+        })
+      }, [])
+      .catch(reject)
+      .then(resolve)
+    },
+
+
+    "archive": function(resolve, reject) {
+      const targets = subtask.argv.targets.split(',').map(function( target ) {
+        if (!COMMANDS[target]) return null
+        return {
+          command: template( COMMANDS[target], { path: subtask.argv.name } ),
+          cwd: subtask.argv.pkg_path,
+          printStdout: false
+        }
+      }).filter(( cmd ) => { return !!cmd })
+
+      wk.exec(targets).catch(reject).then(resolve)
+    }
+  }
+
+}
+
+function command() {
+  this
+  .string('name')
+  .string('targets', 'gzip')
+  .string('pkg_path', path.join(process.cwd(), 'pkg'))
+  .string('include')
+  .string('exclude')
+}
+
+task('default', { command }, function(resolve, reject) {
+  wk.nano
+  .serie(tasks(this), [ 'clean', 'filelist', 'copy', 'archive', 'clean' ])
+  .catch(reject)
+  .then(resolve)
+})
---
+++ tasks/publish.js
@@ -0,0 +1,151 @@
+'use strict'
+
+const fs     = require('fs')
+const path   = require('path')
+const semver = require('semver')
+const name   = path.basename(__filename, path.extname(__filename))
+
+function ns(task) {
+  return `${name}:${task}`
+}
+
+function getPackage() {
+  return JSON.parse(fs.readFileSync(path.resolve('package.json')))
+}
+
+function getCurrentVersion() {
+  return getPackage().version
+}
+
+function prompt( message, cb ) {
+  process.stdin.resume()
+  process.stdin.setEncoding('utf8')
+  process.stdout.write(message)
+  process.stdin.once("data", function (data) {
+    if ( cb ) cb( data.toString().trim() )
+    process.stdin.pause()
+  })
+}
+
+function bump_tasks(subtask) {
+  return {
+    "stage_clean": function(resolve, reject) {
+      wk.exec('git status --porcelain --untracked-files=no', { printStdout: false })
+      .then((result) => {
+        if (result.stdout.length !== 0) {
+          reject(`Stage is not clean`)
+          return
+        }
+        resolve()
+      })
+    },
+
+    "next_version": function(resolve, reject) {
+      const { argv } = subtask
+      const identifier = argv['identifier'] ? argv['identifier'] : null
+      let release      = typeof argv['release'] === 'string' ? argv['release'] : 'patch'
+
+      if (argv['prerelease'] || argv['pre']) release = 'prerelease'
+
+      const nextVersion = semver.inc(getCurrentVersion(), release, identifier)
+      console.log(`Next version: "${nextVersion}"`)
+
+      prompt(`Continue? `, ( answer ) => {
+        if (answer[0] === 'y') {
+          resolve( nextVersion )
+        } else {
+          reject( 'Bump aborted' )
+        }
+      })
+    },
+
+    "bump_version": function(resolve, reject) {
+      const nextVersion = this.result
+
+      if (!nextVersion)
+        return reject('Invalid version :' + nextVersion)
+
+      const pkg   = getPackage()
+      pkg.version = nextVersion
+
+      const pth = path.join('package.json')
+      fs.writeFileSync(pth, JSON.stringify(pkg, true, 2) + '\n')
+
+      resolve( nextVersion )
+    },
+
+    "commit": function(resolve, reject) {
+      const nextVersion = this.result
+
+      const cmds = [
+        `git commit -a -m "Bump ${nextVersion}"`,
+        `git tag -a v${nextVersion} -m "Release ${nextVersion}"`
+      ]
+
+      const opts = {}
+      if (process.platform == 'win32') {
+        opts.windowsVerbatimArguments = true
+      }
+
+      wk.exec(cmds).catch(reject).then(() => {
+        console.log(`Version ${nextVersion} bumped !`)
+        resolve()
+      })
+    }
+  }
+}
+
+function push_tasks(subtask) {
+  return {
+
+    "version_released": function(resolve, reject) {
+
+      wk.exec('git ls-remote', { printStdout: false, printStderr: false })
+      .then((result) => {
+        const version = getCurrentVersion()
+
+        if (result.stdout.match(new RegExp(`\srefs\/tags\/v${version}(\s|$)`))) {
+          return reject(`Version v${version} already released!`)
+        }
+
+        resolve( version )
+      })
+
+    },
+
+    "push": function(resolve, reject) {
+      const version = getPackage().version
+
+      wk.exec(`git push ${subtask.argv.remote} --tag v${version}`)
+        .catch(reject)
+        .then(resolve)
+    }
+
+  }
+}
+
+function command() {
+  this
+  .string('remote', 'origin')
+}
+
+
+task('bump', function(resolve, reject) {
+  wk.nano
+  .serie(bump_tasks(this))
+  .catch(reject)
+  .then(resolve)
+})
+
+task('push', { command }, function(resolve, reject) {
+  if (this.invocator) {
+    this.argv = this.invocator.argv
+  }
+
+  wk.nano
+  .serie(push_tasks(this))
+  .catch(reject)
+  .then(resolve)
+})
+
+task('default', { command }, [ ns('bump'), ns('push') ])
