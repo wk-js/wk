60f9aa8c7376a0b956464daad8419398a28066b4
Christophe Massolin <christophe.massolin@gmail.com>
2017-09-17 05:35:10 +0200

    Fix pool test

--- test/pool.js
+++ test/pool.js
@@ -1,12 +1,12 @@
 'use strict'
 
-const wk = require('../lib/workflow')
+const wk = require('../lib/wk')
 
-wk.task('msg', { async: true, pool: 3 }, function() {
+wk.task('msg', { pool: 3 }, function(resolve) {
   console.log('Start', this.name)
   setTimeout(() => {
     console.log('Done', this.name)
-    this.complete()
+    resolve()
   }, 2000)
 })
 
